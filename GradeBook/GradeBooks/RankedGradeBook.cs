8217a255de401995c976b1ceee6a221bf958434b
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-10-01 15:02:07 +0200

    :tada: Class creation

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public GetLetterGrade()
+        {
+
+        }
+    }
+}
